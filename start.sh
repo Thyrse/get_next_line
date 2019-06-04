5f3d22bdd7f2857f8d999426e465acfeb03cde11
Terry FOURGEUX <tefourge@e3r13p7.42.fr>
2019-06-04 18:06:14 +0200

    Get Next Line 108, refresh code on github

--- start.sh
+++
@@ -1,4 +0,0 @@
-make -C libft/ fclean && make -C libft/
-gcc  -g3 -fsanitize=address -I libft/ -o get_next_line.o -c get_next_line.c
-gcc  -g3 -fsanitize=address -I libft/ -o main.o -c main.c
-gcc  -g3 -fsanitize=address -o test_gnl main.o get_next_line.o -I libft/ -L libft/ -lft
